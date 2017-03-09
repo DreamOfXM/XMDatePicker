6ff3fda8229a842faa04082270b41189cd05e0b2
woaolanqiu123 <guoxm@woaoo.net>
2017-03-09 17:50:57 +0800

    add all files

---
+++ XMDatePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMDatePickerDemo
+//
+//  Created by guo ran on 17/3/9.
+//  Copyright © 2017年 guo xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMDatePickerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMDatePickerDemo
+//
+//  Created by guo ran on 17/3/9.
+//  Copyright © 2017年 guo xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
